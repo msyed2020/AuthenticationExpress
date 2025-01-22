41034a2d0150f3fab82f90f5f3c737781c00d0e6
msyed2020 <mikailsyed@yahoo.com>
2025-01-22 10:56:41 -0500

    Server handling

--- bin/www.js
+++ bin/www.js
@@ -1,1 +1,17 @@
 // separate app logic from actually running app in production, so it depends on each app for sure
+
+var app = require('../app');
+var http = require('http');
+
+// get port from env and store in Express
+
+var port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+// building http server
+var server = http.createServer(app);
+
+// listen on provided port, on all network interfaces
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
