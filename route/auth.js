86c0135dd8f7d05d0133c87d556b2cd6ca3ac51d
msyed2020 <mikailsyed@yahoo.com>
2025-01-26 14:05:07 -0500

    Test auth

--- route/auth.js
+++ route/auth.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var passport = require('passport');
 var localStrategy = require('passport-local');
 var crypto = require('crypto');
-var db = require('/db');
+var db = require('../db');
 
 passport.use(new localStrategy(function verify(username, password, callback) {
     db.get('SELECT * FROM users WHERE username = ?', [username], function(err, row) {
@@ -13,11 +13,27 @@ passport.use(new localStrategy(function verify(username, password, callback) {
         if (!row) {
             return callback(null, false, {message: "Incorrect username or password"});
         }
-    })
+    
+    crypto.pbkdf2(password, row.salt, 310000, 32, 'sha256', function(err, hashedPassword) {
+        if (err) {
+            return callback(err);
+        }
+        if (!crypto.timingSafeEqual(row.hashed_password, hashedPassword)) {
+            return callback(null, false, {message: "Incorrect username or password"});
+        }
+        return callback(null, row);
+    });
+
+    });
 }));
 
 router.get('/login', function(req, res, next) {
     res.render('login');
 });
+
+router.post('/login/password', passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login'
+}))
 
 module.exports = router;
