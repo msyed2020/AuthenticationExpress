4ae6ef7045a236455cfd422235baa7625d4133be
msyed2020 <mikailsyed@yahoo.com>
2025-01-26 13:49:49 -0500

    Editing auth.js

--- route/auth.js
+++ route/auth.js
@@ -1,5 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
+var localStrategy = require('passport-local');
+var crypto = require('crypto');
+var db = require('/db');
+
+passport.use(new localStrategy(function verify(username, password, callback) {
+    
+}));
 
 router.get('/login', function(req, res, next) {
     res.render('login');
