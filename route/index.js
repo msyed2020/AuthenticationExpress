717f753baf2e27b83e4dbd6e4afd828acaa1d78b
msyed2020 <mikailsyed@yahoo.com>
2025-01-27 13:29:25 -0500

    Working through authentication

--- route/index.js
+++ route/index.js
@@ -8,7 +8,11 @@ router.get('/index', function(req, res, next) {
 });
 
 router.get('/', function(req, res) {
-    res.render('home');
+    if (!req.user) {return res.render('home');}
+    next();
+}, function(req, res, next) {
+    res.locals.filter = null;
+    res.render('index', {user: req.user});
 });
 
 module.exports = router;
